71b979d8f2c151a42abb634c6c7b1f2f8cebc029
stambeto09 <stanimir.yakimov@bedegaming.com>
2016-07-16 00:03:33 +0300

    Adds the data access layer

--- src/UniStart/Controllers/StudentsController.cs
+++ src/UniStart/Controllers/StudentsController.cs
@@ -1,13 +1,22 @@
 ﻿using System.Web.Mvc;
+using Unistart.Models;
+using UniStart.Data;
 
 namespace UniStart.Controllers
 {
     public class StudentsController : Controller
     {
+        private UniStartContext context;
+
+        public StudentsController()
+        {
+            context = new UniStartContext();
+        }
         // GET: Students
         public ActionResult Index()
         {
-            return View();
+            context.Lectures.Add(new Lecture() {Title = "First Lecure"});
+            return null;
         }
     }
 }
