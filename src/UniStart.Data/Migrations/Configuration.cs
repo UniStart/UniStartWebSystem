5ff1f8f58fbc9038a894a69602ab2bfd4b15ade1
stambeto09 <stanimir.yakimov@bedegaming.com>
2016-07-16 07:07:33 +0300

    Refactors Migrations

--- src/UniStart.Data/Migrations/Configuration.cs
+++ src/UniStart.Data/Migrations/Configuration.cs
@@ -1,31 +1,16 @@
 namespace UniStart.Data.Migrations
 {
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<UniStart.Data.UniStartContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<UniStartContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
         }
 
-        protected override void Seed(UniStart.Data.UniStartContext context)
+        protected override void Seed(UniStartContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
         }
     }
 }
